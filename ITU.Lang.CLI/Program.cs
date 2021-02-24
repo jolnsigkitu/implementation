6af11c050af9e559648df2e62464e9455a6ef355
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2021-02-24 10:27:01 +0100

    First commit! Add boilerplate

---
+++ ITU.Lang.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITU.Lang.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
