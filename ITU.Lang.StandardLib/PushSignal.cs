2c424204ba99463522321b1bec790928563320cc
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2021-03-24 09:47:14 +0100

    Expose constructor of ProducerSignal through a prettier api
    Co-authored-by: Simon Green Kristensen <me@duckapple.net>

--- ITU.Lang.StandardLib/PushSignal.cs
+++ ITU.Lang.StandardLib/PushSignal.cs
@@ -6,16 +6,15 @@ namespace ITU.Lang.StandardLib
     public abstract class PushSignal<TInput>
     {
         internal abstract void push(TInput value);
+
+        public static ProducerSignal<T> produce<T>(Action<Action<T>> producer) => new ProducerSignal<T>(producer);
     }
 
     public abstract class ChainablePushSignal<TInput, TOutput> : PushSignal<TInput>
     {
         protected IList<PushSignal<TOutput>> next = new List<PushSignal<TOutput>>();
 
-        internal void addNext(PushSignal<TOutput> node)
-        {
-            next.Add(node);
-        }
+        internal void addNext(PushSignal<TOutput> node) => next.Add(node);
 
         public MapSignal<TOutput, TResult> map<TResult>(Func<TOutput, TResult> mapper)
         {
@@ -61,25 +60,19 @@ namespace ITU.Lang.StandardLib
             }
         }
 
-        protected virtual bool shouldPush(TInput value)
-        {
-            return true;
-        }
+        protected virtual bool shouldPush(TInput value) => true;
 
         protected abstract TOutput getNextValue(TInput value);
     }
 
     public class ProducerSignal<TInput> : ChainablePushSignal<TInput, TInput>
     {
-        public ProducerSignal(Action<Action<TInput>> producer)
+        internal ProducerSignal(Action<Action<TInput>> producer)
         {
             producer(push);
         }
 
-        protected override TInput getNextValue(TInput value)
-        {
-            return value;
-        }
+        protected override TInput getNextValue(TInput value) => value;
     }
 
     public class MapSignal<TInput, TOutput> : ChainablePushSignal<TInput, TOutput>
@@ -91,10 +84,7 @@ namespace ITU.Lang.StandardLib
             this.mapper = mapper;
         }
 
-        protected override TOutput getNextValue(TInput value)
-        {
-            return mapper(value);
-        }
+        protected override TOutput getNextValue(TInput value) => mapper(value);
     }
 
     public class ReduceSignal<TInput, TOutput> : ChainablePushSignal<TInput, TOutput>
@@ -125,15 +115,9 @@ namespace ITU.Lang.StandardLib
             this.predicate = predicate;
         }
 
-        protected override bool shouldPush(TInput value)
-        {
-            return predicate(value);
-        }
+        protected override bool shouldPush(TInput value) => predicate(value);
 
-        protected override TInput getNextValue(TInput value)
-        {
-            return value;
-        }
+        protected override TInput getNextValue(TInput value) => value;
     }
 
     public class ForEachSignal<TInput> : PushSignal<TInput>
@@ -145,9 +129,6 @@ namespace ITU.Lang.StandardLib
             this.function = function;
         }
 
-        internal override void push(TInput value)
-        {
-            function(value);
-        }
+        internal override void push(TInput value) => function(value);
     }
 }
